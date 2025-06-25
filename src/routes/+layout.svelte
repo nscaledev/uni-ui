6f56e533573660e793ee8d5d9635c9aaa1278de2
Simon Murray <spjmurray@yahoo.co.uk>
2025-06-25 14:44:10 +0100

    Allow Favicon to be Overridden (#248)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,10 +13,9 @@
 <svelte:head>
 	<title>{env.PUBLIC_TITLE}</title>
 	{#if env.PUBLIC_FAVICON}
-		<link rel="icon" type="image/png" href={env.PUBLIC_FAVICON} />
+		<link rel="shortcut icon" href={env.PUBLIC_FAVICON} />
 	{/if}
 	<script src="/js/iconify-icon-1.0.5.min.js"></script>
-	<script src="/js/svg-inject.min.js"></script>
 </svelte:head>
 
 {@render children?.()}
